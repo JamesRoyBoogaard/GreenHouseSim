017d411458986568da037077a0471d8a4e09756b
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-10-16 22:09:16 +0200

    aligning plants

--- Visuals/main.cpp
+++ Visuals/main.cpp
@@ -1,19 +1,18 @@
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1400, 850), "The Simulation");
 
     sf::RectangleShape background(sf::Vector2f(1400.f, 500.f));
-    sf::RectangleShape greenhouse(sf::Vector2f(800.f, 300.f));
+    sf::RectangleShape garden(sf::Vector2f(800.f, 300.f));
 
     std::vector<sf::Sprite> plants;
-    sf::Sprite soil;
 
     sf::Texture grassTexture;
     sf::Texture plantTexture;
-    //sf::Texture soilTexture;
 
     if (!grassTexture.loadFromFile("../Textures/grass.png", sf::IntRect(0, 0, 1400, 850)))
     {
@@ -23,40 +22,30 @@ int main()
     {
 
     }
-    // if (!soilTexture.loadFromFile("../Textures/dirt.png"))
-    // {
-
-    // }
-
 
     background.setTexture(&grassTexture);
     background.setPosition(0.f,0.f); 
 
-    greenhouse.setFillColor(sf::Color(192, 192, 192));    
-    greenhouse.setOutlineThickness(10.f);
-    greenhouse.setOutlineColor(sf::Color(200, 200, 200)); 
-    greenhouse.setPosition(300.f, 110.f);
+    garden.setFillColor(sf::Color(192, 192, 192));    
+    garden.setOutlineThickness(10.f);
+    garden.setOutlineColor(sf::Color(200, 200, 200)); 
+    garden.setPosition(300.f, 110.f);
 
 
-    float top_row[2] = {50.f,50.f};
-
-    float bottom_row[2] = {100.f,400.f};
+    float top_row[2] = {385.f,188.f};
+    float bottom_row[2] = {435.f,288.f};
 
     for(int i = 0; i<12; i++){
         sf::Sprite plant;
         plant.setTexture(plantTexture);
         plant.setScale(0.2,0.2);
         if(i%2){
-            plant.setPosition(top_row[0] + i*50, top_row[1] );
+            plant.setPosition(top_row[0] + i*50, top_row[1]);
         }else{
             plant.setPosition(bottom_row[0] + i*50, bottom_row[1]);
         }
         plants.push_back(plant);
     }
-
-
-    // soil.setTexture(soilTexture);
-    // soil.setPosition(200.f, 100.f);
 
     while (window.isOpen())
     {
@@ -66,11 +55,13 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        std::cout << "Mouse: " << mousePos.x << ", " << mousePos.y << "\n";
+
 
         window.clear();
         window.draw(background);
-        window.draw(greenhouse);
-        //window.draw(soil);
+        window.draw(garden);
         for(sf::Sprite plant: plants){
             window.draw(plant);
         }
