76af450e2a9457cd93948bcbdb6af4836ee60c76
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-10-16 18:45:07 +0200

    updates to visuals

--- Visuals/main.cpp
+++ Visuals/main.cpp
@@ -3,9 +3,34 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1400, 850), "The Simulattttionnnn");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1400, 850), "The Simulation");
+    sf::RectangleShape background(sf::Vector2f(1300.f, 500.f));
+    sf::Sprite plant;
+    sf::Sprite soil;
+
+    sf::Texture grassTexture;
+    sf::Texture plantTexture;
+    sf::Texture soilTexture;
+
+    if (!grassTexture.loadFromFile("../Textures/grass.png", sf::IntRect(0, 0, 1400, 850)))
+    {
+        
+    }
+    if (!plantTexture.loadFromFile("../Textures/plant_no_bg.png"))
+    {
+
+    }
+    if (!plantTexture.loadFromFile("../Textures/dirt.png"))
+    {
+
+    }
+
+
+    background.setTexture(&grassTexture);
+    background.setOutlineThickness(10.f);
+    background.setOutlineColor(sf::Color(192, 192, 192)); // steel-like gray  
+    background.setPosition(10.f,10.f);  
+    plant.setTexture(plantTexture);
 
     while (window.isOpen())
     {
@@ -17,7 +42,8 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(background);
+        window.draw(plant);
         window.display();
     }
 
