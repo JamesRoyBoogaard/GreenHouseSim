a205dcf2798fda8774612777d2b8fb3658834037
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-10-17 18:02:23 +0200

    explaining the line concept a bit more clearly

--- src/Airflow.cpp
+++ src/Airflow.cpp
@@ -9,7 +9,8 @@ void Handle_line_Collision();
 float Diminish_Velocity(const Airflow::Line& line);
 void Create_Outlet_Lines(float reach, float strength, sf::Vector2f position);
 
-
+// The intention for any kind of line is to create a particle that follows a tragectery and lifecycle and then slows and diminshes visually. 
+// This applies to both the Directional and Offshoot lines, they are not lines but dots that have followed a tragectory and left a trail.
 
 Airflow::Airflow() {
 
