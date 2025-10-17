14905eaa4a28b7108668dea07b85175f6b7ed8f0
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-10-17 17:58:51 +0200

    making the header file correct and neater. Did this by moving all the structs to the header as there is inheritence between Line and its subs

--- include/Airflow.hpp
+++ include/Airflow.hpp
@@ -4,9 +4,38 @@
 class Airflow {
     public:
         Airflow();
-        struct Origin;
+        struct Origin
+        {
+            int fan_rate;
+            sf::Vector2f position;
+        };
+
+        struct Line
+        {
+            float velocity;
+            sf::Vector2f position;
+            float rate_of_slowing;
+        };
+
+        struct Outlet
+        {
+            float reach;
+            float strength; 
+            sf::Vector2f position;
+        };
+
+        struct Directional_line: public Line
+        {
+            
+        };
+
+        struct Offshoot_line: public Line
+        { 
+            float tightness_of_curve;
+        };
 
     private:
+
 
 
 };
--- src/Airflow.cpp
+++ src/Airflow.cpp
@@ -1,42 +1,17 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Airflow.hpp"
 
 void Move_Directional_Line();
 void Create_Directional_Line(sf::Vector2f direction, sf::Vector2f initial_position); //Spawn a directional line that then travels in the target
 void Create_Offshoot_lines();
 void Handle_line_Collision();
-struct Line;
-float Diminish_Velocity(Line line);
+float Diminish_Velocity(const Airflow::Line& line);
 void Create_Outlet_Lines(float reach, float strength, sf::Vector2f position);
 
-struct Origin
-{
-    int fan_rate;
-    sf::Vector2f postion;
-};
 
-struct Line
-{
-    float velocity;
-    sf::Vector2f position;
-    float rate_of_slowing;
 
-};
+Airflow::Airflow() {
 
-struct Oulet
-{
-    float reach;
-    float strength; 
-    sf::Vector2f position;
-};
-
-struct Directional_line: public Line
-{
-    
-};
-
-struct Offshoot_line: public Line
-{ 
-    float tightness_of_curve;
-};
+}
 
