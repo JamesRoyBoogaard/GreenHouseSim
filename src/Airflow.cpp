45ed436c2831ea71f6f9d44d51c88f82c0c36da6
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-10-17 17:42:36 +0200

    not ended i guess XD. cleaning up the Airflow class by declaring Line earlier on in the file

--- src/Airflow.cpp
+++ src/Airflow.cpp
@@ -5,21 +5,22 @@ void Move_Directional_Line();
 void Create_Directional_Line(sf::Vector2f direction, sf::Vector2f initial_position); //Spawn a directional line that then travels in the target
 void Create_Offshoot_lines();
 void Handle_line_Collision();
-struct Line
-{
-    float velocity;
-    sf::Vector2f position;
-    float rate_of_slowing;
-
-};
+struct Line;
 float Diminish_Velocity(Line line);
-
 void Create_Outlet_Lines(float reach, float strength, sf::Vector2f position);
 
 struct Origin
 {
     int fan_rate;
     sf::Vector2f postion;
+};
+
+struct Line
+{
+    float velocity;
+    sf::Vector2f position;
+    float rate_of_slowing;
+
 };
 
 struct Oulet
