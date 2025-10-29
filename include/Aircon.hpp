cb21ce85fa27671efd1e0ae0a35767dceecab3e4
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-10-29 15:43:10 +0100

    updates made t the aircon class

--- include/Aircon.hpp
+++ include/Aircon.hpp
@@ -5,12 +5,19 @@
 
 class Aircon{
 
-    Aircon(sf::Vector2f& position,sf::Vector2f& direction, sf::RenderWindow& window, sf::Vector2f& intake_speed);
     public:
+    Aircon(sf::Vector2f& position, sf::Vector2f p_direction, sf::RenderWindow& p_window, float p_speed);
+
+    sf::RenderWindow& window;
+    sf::Vector2f direction;
+    float speed;
     sf::RectangleShape aircon_box;
     sf::Vector2f intake;
     sf::Vector2f outake;
     sf::Vector2f offset_intake;
+
+    void draw();
+    void airflow();
 
     private:
     
--- src/Aircon.cpp
+++ src/Aircon.cpp
@@ -1,7 +1,30 @@
 #include "Aircon.hpp"
 
 
-Aircon::Aircon(sf::Vector2f& position, sf::Vector2f& direction, sf::RenderWindow& window, sf::Vector2f& intake_speed){
+Aircon::Aircon(sf::Vector2f& position,
+               sf::Vector2f p_direction,
+               sf::RenderWindow& p_window,
+               float p_speed)
+    : window(p_window),
+    aircon_box({50.f, 50.f})           
+{
+    aircon_box.setPosition(position);
+    aircon_box.setFillColor(sf::Color::Blue);
+    intake = position;
+    direction = p_direction;
+    speed = p_speed;
+    outake = position;
+    offset_intake = intake;//come up with better way to use the speed to determine how far the offset is
+}
 
+void Aircon::draw(){
 
+    
 };
+
+void Aircon::airflow(){
+    // work out direction and then add speed to the 2dvector direction to form the velocity 
+    sf::Vector2f velocity = {direction.x*speed, direction.y*speed};
+    //just copy accross how the dots are spawining with the airflow class in main and it should work if you then let this class handle the logic
+
+}
